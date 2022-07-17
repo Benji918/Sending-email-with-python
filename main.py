a04914cd8dcfb69a0d709acebf55162f2c380dec
Benjamin <70521283+Benji918@users.noreply.github.com>
2022-07-17 14:01:56 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import smtplib
+import datetime as dt
+import random
+my_email = 'kodiugos@gmail.com'
+pwd = 'llhytkakbfhnikci'
+date = dt.datetime.now()
+week = date.weekday()
+print(week)
+if week == 5:
+    with open('quotes.txt') as quotes_file:
+        quotes = quotes_file.readlines()
+        random_quote = random.choice(quotes)
+    # Connect to  email server provider
+    with smtplib.SMTP('smtp.gmail.com') as connection:
+        # secure connection
+        connection.starttls()
+        # login to email
+        connection.login(user=my_email, password=pwd)
+        # send an email
+        connection.sendmail(from_addr=my_email,
+                            to_addrs='ugochukwub79@gmail.com',
+                            msg=f'Subject:Motivational Quote of the day!\n\n{random_quote}')
+
+
+# now = dt.datetime.now()
+# day_of_wk = now.weekday()
+# print(day_of_wk)
+#
+# day_of_birth = dt.datetime(year=2002, month=9, day=18, hour=4, minute=45)
+# print(day_of_birth)
